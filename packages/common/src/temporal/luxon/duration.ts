135cbe8ae89960eec7eadaf6bcace2faff01d7e3
Carson Full <carson_full@tsco.org>
2023-03-20 18:11:15 -0500

    feat(common): add optional Luxon `Duration.from` addition
    This is accessed by with `import '@seedcompany/common/temporal/luxon';`

---
+++ packages/common/src/temporal/luxon/duration.ts
@@ -0,0 +1,44 @@
+import { parseHumanToDurationLike } from '@seedcompany/common';
+import { Duration, DurationLike } from 'luxon';
+import { Writable } from 'type-fest';
+
+/**
+ * A duration represented as an:
+ * - ISO string {@link Duration.fromISO}
+ * - Human string {@link Duration.fromHuman}
+ * - millisecond number {@link Duration.fromMillis}
+ * - object literal {@link Duration.fromObject}
+ * - Duration instance
+ */
+export type DurationIn = string | DurationLike;
+
+declare module 'luxon/src/duration' {
+  // eslint-disable-next-line @typescript-eslint/no-namespace -- augmenting static class method
+  namespace Duration {
+    /**
+     * Create from ISO, Human, ms number, std object, or another Duration.
+     */
+    export const from: (duration: DurationIn) => Duration;
+    /**
+     * Parse a humanized duration string.
+     * @example
+     * '1hour 20mins'
+     * '27,681 ms'    // numeric separators
+     * '2hr -40mins'  // negatives
+     * '2e3 secs'     // exponents
+     */
+    export const fromHuman: (duration: string) => Duration;
+  }
+}
+
+const D = Duration as Writable<typeof Duration>;
+
+D.from = (input: DurationIn) =>
+  typeof input === 'string'
+    ? input.startsWith('P') || input.startsWith('-P')
+      ? D.fromISO(input)
+      : D.fromHuman(input)
+    : D.fromDurationLike(input);
+
+D.fromHuman = (input: string) =>
+  Duration.fromObject(parseHumanToDurationLike(input));
