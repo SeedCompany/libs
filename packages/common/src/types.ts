be0ebf7f27ea43b09bf97eef6a01fbe59261dc61
Carson Full <carson_full@tsco.org>
2023-03-25 09:24:53 -0500

    feat(common): add `IterableItem` type helper

--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -2,4 +2,12 @@ export type MaybeAsync<T> = T | Promise<T>;
 
 export type Nil = null | undefined;
 
-export type ArrayItem<T> = T extends ReadonlyArray<infer U> ? U : never;
+export type IteratorItem<T> = T extends Iterator<infer Item> ? Item : never;
+
+export type IterableItem<T> = T extends {
+  [Symbol.iterator]: () => infer TIterator;
+}
+  ? IteratorItem<TIterator>
+  : never;
+
+export type ArrayItem<T> = IterableItem<T>;
