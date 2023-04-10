14295a09adaf20d7e8bf2ee21ef82fb30e1fd3c9
Carson Full <carson_full@tsco.org>
2023-04-10 18:28:47 -0500

    feat(common): add setHas helper along with WidenLiteral

---
+++ packages/common/src/set-has.ts
@@ -0,0 +1,25 @@
+import { WidenLiteral } from './types';
+
+/**
+ * A type helper to check if a set has a key
+ * whose key is a bit looser than a set of literals.
+ *
+ * @example
+ * const colors = new Set(['red', 'green', 'blue'] as const);
+ * const userInput: string = 'blue';
+ *
+ * // This errors because userInput is not a color literal.
+ * const validColor = colors.has(userInput);
+ *
+ * // This works
+ * const validColor = setHas(colors, userInput);
+ *
+ * // It also narrows the key to the literal type
+ * if (validColor) {
+ *   // userInput is now 'red' | 'green' | 'blue'
+ * }
+ */
+export const setHas = <T>(
+  set: ReadonlySet<T & {}>,
+  key: WidenLiteral<T> & {},
+): key is WidenLiteral<T> & {} & T => set.has(key as any);
--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -23,3 +23,15 @@ export type FnLike = (...args: any[]) => any;
  * A type safe function whose signature is unknown.
  */
 export type UnknownFn = (...args: unknown[]) => unknown;
+
+export type WidenLiteral<T> = T extends string
+  ? string
+  : T extends number
+  ? number
+  : T extends boolean
+  ? boolean
+  : T extends bigint
+  ? bigint
+  : T extends symbol
+  ? symbol
+  : T;
