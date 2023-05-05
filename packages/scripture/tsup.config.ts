f09a82221ffe092d4ab66455789f67f3b64f5e15
Carson Full <carson_full@tsco.org>
2023-05-05 15:30:14 -0500

    fix(scripture): transpile es2020
    our UIs still use webpack v4
    Just time constrained right now, so this is easiest.

--- packages/scripture/tsup.config.ts
+++ packages/scripture/tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'tsup';
 export const tsup = defineConfig({
   entry: ['src/index.ts', '!src/**/*.test.ts'],
   format: ['cjs', 'esm'],
+  target: 'es2019',
   dts: true,
   sourcemap: true,
   clean: true,
