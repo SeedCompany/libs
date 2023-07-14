5375f769542b74a33c962181cf9d8e8308e8a2d2
Carson Full <carson_full@tsco.org>
2023-07-13 19:18:48 -0500

    fix(email): jsx compilation in built files

--- packages/email/tsup.config.ts
+++ packages/email/tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'tsup';
 export const tsup = defineConfig({
   entry: ['src/index.ts', 'src/templates/index.ts', '!src/**/*.test.ts'],
   format: ['cjs', 'esm'],
+  inject: ['./react-shim.ts'],
   dts: true,
   sourcemap: true,
   clean: true,
