9bb254e82032c3acf8e83caaa00cc0bf011809b4
Carson Full <carson_full@tsco.org>
2023-07-19 10:22:36 -0500

    fix(email): drop ESM support
    This wasn't actually being used yet.
    We'll do a complete migration with a major in the future.

--- packages/email/tsup.config.ts
+++ packages/email/tsup.config.ts
@@ -3,12 +3,8 @@ import { defineConfig } from 'tsup';
 export const tsup = defineConfig({
   entry: ['src', '!src/**/*.test.ts'],
   bundle: false,
-  format: ['cjs', 'esm'],
+  format: ['cjs'],
   inject: ['./react-shim.ts'],
-  dts: true,
-  outExtension: ({ format }) => ({
-    js: format === 'cjs' ? '.cjs' : '.mjs',
-  }),
   sourcemap: true,
   clean: true,
 });
