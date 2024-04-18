0d47cf47898fbe24f3adb8fdf4cb000b40f68a89
Carson Full <carson_full@tsco.org>
2024-04-18 14:01:06 -0500

    fix: sourcemap paths (#32)

--- packages/common/tsup.config.ts
+++ packages/common/tsup.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'tsup';
 
 export const tsup = defineConfig({
   entry: ['src/index.ts', 'src/temporal/luxon/index.ts', '!src/**/*.test.ts'],
+  esbuildOptions: (opts) => {
+    opts.sourceRoot = './dist/';
+  },
   external: ['@seedcompany/common'],
   format: ['cjs', 'esm'],
   dts: true,
--- packages/scripture/tsup.config.ts
+++ packages/scripture/tsup.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'tsup';
 
 export const tsup = defineConfig({
   entry: ['src/index.ts', '!src/**/*.test.ts'],
+  esbuildOptions: (opts) => {
+    opts.sourceRoot = './scripture/dist/';
+  },
   format: ['cjs', 'esm'],
   target: 'es2019',
   dts: true,
