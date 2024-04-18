75025c4f2d15d8ad23e3be45279a35b0409960e1
Carson Full <carson_full@tsco.org>
2024-04-18 13:52:14 -0500

    fix: sourcemap paths
    Since we build to dist/ but then change to publish dist/
    as the package root, the path was messed up.
    It has `../src` which normalizes to removing the package name.
    i.e. `node_modules/@seedcompany/src/...`.
    Setting the `sourceRoot` changes path to `./dist/../src`
    to cancel this out.

--- packages/cache/tsup.config.ts
+++ packages/cache/tsup.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'tsup';
 
 export const tsup = defineConfig({
   entry: ['src', '!src/**/*.test.ts'],
+  esbuildOptions: (opts) => {
+    opts.sourceRoot = './dist/';
+  },
   format: ['cjs', 'esm'],
   dts: true,
   sourcemap: true,
