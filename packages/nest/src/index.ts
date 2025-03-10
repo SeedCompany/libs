c566cdcb8a4bfd66b0a3a2c0d2276a68f553d00b
Carson Full <carson_full@tsco.org>
2025-03-10 10:21:59 -0500

    feat(nest): patchDecoratedMethod (#43)
    A small wrapper that forwards method metadata to new function

--- packages/nest/src/index.ts
+++ packages/nest/src/index.ts
@@ -2,3 +2,4 @@ export * from './dep-injector.js';
 export * from './make-enum.js';
 export * from './repl.js';
 export * from './metadata-decorator.js';
+export * from './patch-decorated-method.js';
