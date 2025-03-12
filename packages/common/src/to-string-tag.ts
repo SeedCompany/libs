56187d4b2fb25faf167e268effd7ad173e8cb154
Carson Full <carson_full@tsco.org>
2025-03-12 14:51:48 -0500

    feat(common): setToStringTag

--- packages/common/src/to-string-tag.ts
+++ packages/common/src/to-string-tag.ts
@@ -1,3 +1,5 @@
+import { defineClassProp } from './define-class-prop.js';
+
 /**
  * Get the string representation of the object.
  *
@@ -16,3 +18,28 @@
  */
 export const toStringTag = (value: unknown) =>
   Object.prototype.toString.call(value).slice(8, -1);
+
+/**
+ * A small helper to define the {@link Symbol.toStringTag} for the class or object.
+ *
+ * Useful to convey that the class/object is not a "regular" object.
+ * @see isRegularObject
+ */
+export const setToStringTag = <
+  Cls extends { prototype: unknown } | object,
+  T = Cls extends { prototype: infer I } ? I : Cls,
+>(
+  classOrObject: Cls,
+  tag: string | ((this: T, instance: T) => string),
+) =>
+  defineClassProp(
+    classOrObject,
+    Symbol.toStringTag,
+    typeof tag === 'string'
+      ? { value: tag }
+      : {
+          get: function toStringTag(this: T) {
+            return tag.call(this, this);
+          },
+        },
+  );
