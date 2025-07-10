83ea7d04c952c6efb14763a5eae826b409fe53bc
Carson Full <carson_full@tsco.org>
2025-07-10 11:31:03 -0500

    feat(email): re-export usePromise helper

--- packages/email/src/templates/index.ts
+++ packages/email/src/templates/index.ts
@@ -2,3 +2,4 @@ export * from './mjml.js';
 export { Attachment } from './attachment.js';
 export { HideInText, inText, InText } from './text-rendering.js';
 export { Title } from './title.js';
+export { default as usePromise } from 'react-promise-suspense';
