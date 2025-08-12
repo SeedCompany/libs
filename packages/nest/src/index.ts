37e4017f05a23fb3d1e3c1cc5110ebc9d938dfe3
Carson Full <carson_full@tsco.org>
2025-08-12 14:22:26 -0500

    feat(nest): add validator module to register global pipe & support injectable constraints (#57)

--- packages/nest/src/index.ts
+++ packages/nest/src/index.ts
@@ -4,3 +4,4 @@ export * from './repl.js';
 export * from './metadata-decorator.js';
 export * from './patch-decorated-method.js';
 export { markSkipClassTransformation } from './patches/class-transformer.patch.js';
+export { ValidatorModule } from './validator/validator.module.js';
