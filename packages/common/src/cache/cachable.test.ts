6591b689a63d7dc46306fc76d062378598ad120d
Carson Full <carson_full@tsco.org>
2025-10-09 19:41:07 -0500

    test(common): alt cacheable signature

--- packages/common/src/cache/cachable.test.ts
+++ packages/common/src/cache/cachable.test.ts
@@ -8,3 +8,13 @@ test('cachable', () => {
   expect(cached.cache).toBe(cache);
   expect(cached('key')).toBe('key');
 });
+
+test('cachable inferred signatures', () => {
+  const cached = cacheable((key: 'red') => ({ key }))(new Map());
+  expect(cached('red').key).toBe('red');
+  // @ts-expect-error the key should be strict
+  cached('blue');
+  // @ts-expect-error the key should be strict
+  // eslint-disable-next-line @typescript-eslint/no-unused-expressions,@typescript-eslint/no-unnecessary-condition
+  cached('red').key === 'blue';
+});
